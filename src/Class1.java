8e05e9f8d59baa4f5e05a8e351ef8b4ef295c115
Brandon Woolard <brandon.woolard.2019@student.lhschools.org>
2018-09-07 10:00:53 -0400

    Started Code

---
+++ src/Class1.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class Class1 {
+    public static void main(String[] args) {
+        int numberOne;
+        int numberTwo;
+        int numberThree;
+        int numberFour;
+        int numberFive;
+        int numberSix;
+        int numberSeven;
+        int numberEight;
+        int numberNine;
+        int numberTen;
+
+        Scanner keyboard = new Scanner(System.in);
+
+        numberOne = 1;
+        numberTwo = 2;
+        numberThree = 3;
+        numberFour = 4;
+        numberFive = 5;
+        numberSix = 6;
+
+    }
+}
