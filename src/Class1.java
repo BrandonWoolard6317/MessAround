164a5bbd0310319683e3faf13e62ea6c87d01e3a
Brandon Woolard <42773989+BrandonWoolard6317@users.noreply.github.com>
2018-09-07 13:20:11 -0400

    Added $

--- src/Class1.java
+++ src/Class1.java
@@ -57,10 +57,10 @@ public class CheckFromWeek {
         afterTax = (beforeTax - Tax) - unionFees;
         
         System.out.println("Name: Brandon Woolard")
-        System.out.println("Hours Worked: " + Hours)
-        System.out.println("Before Tax: " + beforeTax)
-        System.out.println("Tax: -" + Tax)
-        System.out.println("Union Fees: -" + unionFees)
-        System.out.println("After Taxes: " + afterTax)
+        System.out.println("Hours Worked: " + Hours " hours)
+        System.out.println("Before Tax: $" + beforeTax)
+        System.out.println("Tax: -$" + Tax)
+        System.out.println("Union Fees: -$" + unionFees)
+        System.out.println("After Taxes: $" + afterTax)
     }   
 }
