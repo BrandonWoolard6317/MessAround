bc4f986505ea051f3e54a5b00809c2d24abf847e
Brandon Woolard <42773989+BrandonWoolard6317@users.noreply.github.com>
2018-09-10 11:22:06 -0400

    Clean

--- src/Class1.java
+++ src/Class1.java
@@ -1,38 +1,5 @@
 import java.util.Scanner;
 
-public class Class1 {
-    public static void main(String[] args) {
-        int numberOne;
-        int numberTwo;
-        int numberThree;
-        int numberFour;
-        int numberFive;
-        int numberSix;
-        int numberSeven;
-        int numberEight;
-        int numberNine;
-        int numberTen;
-
-        Scanner keyboard = new Scanner(System.in);
-
-        numberOne = 1;
-        numberTwo = 2;
-        numberThree = 3;
-        numberFour = 4;
-        numberFive = 5;
-        numberSix = 6;
-        numberSeven = 7;
-        numberEight = 8;
-        numberNine = 9;
-        numberTen = 10;
-        
-        System.out.println("");
-
-    }
-}
-
-import java.util.Scanner;
-
 public class NumberGuessingGame {
 	public static void main(String[] args){
 		String
