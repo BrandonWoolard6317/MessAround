6f3ba48888792b083ccc5914e743a52031acdd7c
Brandon Woolard <42773989+BrandonWoolard6317@users.noreply.github.com>
2018-09-10 13:16:35 -0400

    Finished If statements-Needs to be tested

--- src/Class1.java
+++ src/Class1.java
@@ -31,10 +31,6 @@ public class NumberGuessingGame {
 		if else(numberGuessing > generatedNumber){
 			System.out.println("Your guess is over the number! You have 3 more attempts.\nTry again.");
 			usersInput = keyboard.nextLine();
-		}
-		if else(numberGuessing < generatedNumber){
-			System.out.println("Your guess is under the number! You have 3 more attempts.\nTry again.");
-			usersInput = keyboard.nextLine();
 			if(numberGuessing == generatedNumber){
 				System.out.println("Congrats! You've guessed "+generatedNumber+" correct on\nyour "+
 						   "second try! Can you do it again?");
@@ -55,6 +51,49 @@ public class NumberGuessingGame {
 				if else(numberGuessing > generatedNumber){
 		        	        System.out.println("Your guess is over the number! You only have one attempt left.\nTry again.");
 		        	        usersInput = keyboard.nextLine();
+				}
+				if else(numberGuessing < generatedNumber){
+		        	        System.out.println("Your guess is under the number! You only have one attempt left.\nTry again.");
+		        	        usersInput = keyboard.nextLine();
+					if(numberGuessing == generatedNumber){
+						System.out.println("Congrats! You've guessed "+generatedNumber+" correct on\nyour "+
+						   "last try! Can you do it again?");
+				                System.exit(0)
+					}
+					if else(numberGuessing > generatedNumber){
+		        	                System.out.println("Your guess is over the number! Sorry that was your last\ntry."+
+								   "The number was "+generatedNumber+". Feel free to play again!");
+		        	                System.exit(0)
+					}
+					if else(numberGuessing < generatedNumber){
+		        	                System.out.println("Your guess is under the number! Sorry that was your last\ntry."+
+								   "The number was "+generatedNumber+". Feel free to play again!");
+		        	                System.exit(0)
+					}
+	                        	}
+		                        if else(numberGuessing < generatedNumber){
+	                 	        	System.out.println("Your guess is under the number! You have 3 more attempts.\nTry again.");
+	                 	        	usersInput = keyboard.nextLine();
+	                		if(numberGuessing == generatedNumber){
+	         		        	System.out.println("Congrats! You've guessed "+generatedNumber+" correct on\nyour "+
+					         	           "second try! Can you do it again?");
+			                 	System.exit(0)
+	                 		}
+	                 		if else(numberGuessing > generatedNumber){
+         		                 	System.out.println("Your guess is over the number! You have 2 more attempts.\nTry again.");
+	            	                	usersInput = keyboard.nextLine();
+                   	    		}
+	                 		if else(numberGuessing < generatedNumber){
+	         	                  	System.out.println("Your guess is under the number! You have 2 more attempts.\nTry again.");
+		                         	usersInput = keyboard.nextLine();
+	         			if(numberGuessing == generatedNumber){
+		         		        System.out.println("Congrats! You've guessed "+generatedNumber+" correct on\nyour "+
+		          				   "third try! Can you do it again?");
+				                System.exit(0)
+	         			}
+	         			if else(numberGuessing > generatedNumber){
+		                 	        System.out.println("Your guess is over the number! You only have one attempt left.\nTry again.");
+		                 	        usersInput = keyboard.nextLine();
 				}
 				if else(numberGuessing < generatedNumber){
 		        	        System.out.println("Your guess is under the number! You only have one attempt left.\nTry again.");
