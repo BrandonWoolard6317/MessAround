e5b8c96cef86f827a408e160e956e65a71c620e8
Brandon Woolard <42773989+BrandonWoolard6317@users.noreply.github.com>
2018-09-10 11:36:59 -0400

    Created Number Generator

--- src/Class1.java
+++ src/Class1.java
@@ -2,7 +2,14 @@ import java.util.Scanner;
 
 public class NumberGuessingGame {
 	public static void main(String[] args){
-		String
+		//Declaring variables
+		int generatedNumber;
+		
+		//Generating random number
+		generatedNumber = (int) (Math.random()*100);
+		System.out.println(generatedNumber);
+		
+		
 	
   
         
