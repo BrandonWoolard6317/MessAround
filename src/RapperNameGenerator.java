850ab4b22970c401ea99b71059bc2254548aadc9
Brandon Woolard <brandon.woolard.2019@student.lhschools.org>
2018-09-20 10:32:54 -0400

    Added Album and Song Name

--- src/RapperNameGenerator.java
+++ src/RapperNameGenerator.java
@@ -7,8 +7,16 @@ public class RapperNameGenerator {
 
         Scanner input;
         String personsName;
-        String[] firstName;
         String[] Adjective;
+        String[] Object;
+        String rapperName;
+        String[] albumFirstPart;
+        String[] albumSecondPart;
+        String albumName;
+        String[] songFirstPart;
+        String[] songSecondPart;
+        String songName;
+
         Random random = new Random();
         int otherNumber = random.nextInt(5);
         int number2 = random.nextInt(5);
@@ -25,18 +33,59 @@ public class RapperNameGenerator {
         Adjective[3] = "Crystal";
         Adjective[4] = "Shiny";
 
-        firstName = new String[5];
-        firstName[0] = "Shelf" ;
-        firstName[1] = "Mountain";
-        firstName[2] = "Sharktooth";
-        firstName[3] = "Silver";
-        firstName[4] = "Oompa";
+        Object = new String[5];
+        Object[0] = "Shelf" ;
+        Object[1] = "Mountain";
+        Object[2] = "Sharktooth";
+        Object[3] = "Silver";
+        Object[4] = "Oompa";
 
+        rapperName = Adjective[number2]+" "+Object[number2];
 
+        albumFirstPart = new String[5];
+        albumFirstPart[0] = "Never Ending";
+        albumFirstPart[1] = "Undying";
+        albumFirstPart[2] = "Weird";
+        albumFirstPart[3] = "Sharp";
+        albumFirstPart[4] = "Creepy";
 
-        System.out.println("Ok "+personsName+", are you ready to be a rapper? You first need a rapper name which I " +
-                "can help you with.\nYour rapper name will be "+Adjective[number2] +" "+ firstName[otherNumber]);
+        albumSecondPart = new String[5];
+        albumSecondPart[0] = "Love";
+        albumSecondPart[1] = "Money";
+        albumSecondPart[2] = "Skills";
+        albumSecondPart[3] = "Personality";
+        albumSecondPart[4] = "Heart";
 
-        //System.out.println(firstName[0]);
+        songFirstPart = new String[5];
+        songFirstPart[0] = "Quick";
+        songFirstPart[1] = "Smelly";
+        songFirstPart[2] = "Bright";
+        songFirstPart[3] = "Inside";
+        songFirstPart[4] = "Pringle";
+
+        songSecondPart = new String[5];
+        songSecondPart[0] = "Reflexes";
+        songSecondPart[1] = "Heartbeat";
+        songSecondPart[2] = "Greetings";
+        songSecondPart[3] = "Queue";
+        songSecondPart[4] = "Man";
+
+        //Displays Rapper Name
+        System.out.println("Ok "+personsName+", are yo ready to be a rapper? Yo first need a rapper name which I " +
+                "can help yo with.\nYour rapper name will be "+Adjective[number2] +" "+ Object[otherNumber]);
+
+        //Displays Rapper's Albums Name
+        System.out.println("Now we need to make your first album name.\nDon't worry I'll name if for yo.");
+        System.out.print("Yo album name is "+rapperName+".");
+
+        //Displays Rapper's First Song Name
+        System.out.println(" Great! Now that we've figured out the Album's name, let's figure out yo first songs' " +
+                "name.");
+        System.out.println("Aha it'll be named "+songFirstPart[number2]+" "+songSecondPart[number2]+"!");
+
+        //Displaying Information of The First Song
+        System.out.println("Now that we've figure out all of yo wants and needs to start yo rappa career, lets see what" +
+                "it looks like. ");
+        System.out.println("Rapper Name: "+rapperName);
     }
 }
